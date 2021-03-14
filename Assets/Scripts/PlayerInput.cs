0b9cc974e2c614bbdfc0e5be5057345eb3ba28a1
Tuttep Tanwattanakul <tutteptan@kkumail.com>
2021-03-14 18:40:06 +0700

    Added Input Manager

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,267 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInput"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerMain"",
+            ""id"": ""4da35987-78ca-4f6b-aeb7-f80be66ba970"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""d63c7af5-6f01-4d97-979e-64f136b19005"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""8373d7cc-3918-40c8-b6d9-308e0d79c2b3"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Dash"",
+                    ""type"": ""Button"",
+                    ""id"": ""4fb9c79a-5676-4e1f-b95d-3364854fcce7"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""533aa617-0f8f-4aaf-85bd-04d1cacc4b06"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""a59287c1-8677-4f73-8dce-a5a044c1d149"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""fb87ce4c-5bc8-433b-a7ef-b879e4bf2712"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""9693d190-5b43-4e8c-a731-6154ee7d7409"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""85bd9dd7-419b-45a8-8a62-c4cae4f3168f"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""cbe78a2f-cc03-45ee-a7fc-595564e8f636"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""25259b79-3729-408e-b5b0-39b7a441daa6"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""b95a4080-3a76-4b60-887e-a675005973ed"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d7a5cb36-adf4-45e5-814e-c9b4e3327d32"",
+                    ""path"": ""<Gamepad>/buttonNorth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""60d95fdc-3982-4f0b-a64b-ba4b44f8c8bc"",
+                    ""path"": ""<Keyboard>/leftShift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // PlayerMain
+        m_PlayerMain = asset.FindActionMap("PlayerMain", throwIfNotFound: true);
+        m_PlayerMain_Move = m_PlayerMain.FindAction("Move", throwIfNotFound: true);
+        m_PlayerMain_Jump = m_PlayerMain.FindAction("Jump", throwIfNotFound: true);
+        m_PlayerMain_Dash = m_PlayerMain.FindAction("Dash", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerMain
+    private readonly InputActionMap m_PlayerMain;
+    private IPlayerMainActions m_PlayerMainActionsCallbackInterface;
+    private readonly InputAction m_PlayerMain_Move;
+    private readonly InputAction m_PlayerMain_Jump;
+    private readonly InputAction m_PlayerMain_Dash;
+    public struct PlayerMainActions
+    {
+        private @PlayerInput m_Wrapper;
+        public PlayerMainActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_PlayerMain_Move;
+        public InputAction @Jump => m_Wrapper.m_PlayerMain_Jump;
+        public InputAction @Dash => m_Wrapper.m_PlayerMain_Dash;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerMain; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerMainActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerMainActions instance)
+        {
+            if (m_Wrapper.m_PlayerMainActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnJump;
+                @Dash.started -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnDash;
+                @Dash.performed -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnDash;
+                @Dash.canceled -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnDash;
+            }
+            m_Wrapper.m_PlayerMainActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Dash.started += instance.OnDash;
+                @Dash.performed += instance.OnDash;
+                @Dash.canceled += instance.OnDash;
+            }
+        }
+    }
+    public PlayerMainActions @PlayerMain => new PlayerMainActions(this);
+    public interface IPlayerMainActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnDash(InputAction.CallbackContext context);
+    }
+}
